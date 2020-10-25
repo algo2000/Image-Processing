1ef30adeadb6b733d197257429a7313a4762e468
leo <algo2000@naver.com>
2020-10-26 02:31:35 +0900

    update ch06/main.cpp
    main 수정
    예제 코드 추가

--- ch06/main.cpp
+++ ch06/main.cpp
@@ -25,7 +25,52 @@ string imgFolderLink = "/Users/leo/Documents/학교 문서/ᄋ
 
 int main()
 {
-    ex05();
+    ex07();
+}
+
+int ex07()
+{
+    Mat image = imread(imgFolderLink + "lenna.jpg",1);
+    imshow("원본이미지",image);
+    waitKey(0);
+
+    GaussianBlur(image, image, Size(3, 3), 0, 0, BORDER_DEFAULT);
+    imshow("가우시안블러 적용시",image);
+    waitKey(0);
+
+    Sobel(image, image,CV_8U,2,0,1,1,0,BORDER_DEFAULT);
+    imshow("원본이미지 sobel operation", image);
+    waitKey(0);
+}
+
+int ex06()
+{
+    Mat src;
+    Mat grad;
+    int scale = 1;
+    int delta = 0;
+    src = imread(imgFolderLink + "lenna.jpg", IMREAD_GRAYSCALE);
+    if (src.empty())
+    {
+        return -1;
+    }
+
+    Mat grad_x, grad_y;
+    Mat abs_grad_x, abs_grad_y;
+
+    Sobel(src,grad_x,CV_16S,1,0,3,scale,delta,BORDER_DEFAULT);
+    Sobel(src,grad_y,CV_16S,1,0,3,scale,delta,BORDER_DEFAULT);
+
+    convertScaleAbs(grad_x,abs_grad_x);
+    convertScaleAbs(grad_y,abs_grad_y);
+
+    addWeighted(abs_grad_x,0.5,abs_grad_y,0.5,0,grad);
+
+    imshow("Image",src);
+    imshow("Sobel",grad);
+
+    waitKey(0);
+    return 0;
 }
 
 int ex05()
